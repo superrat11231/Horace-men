fb033dfbeb7068adeb9bb364e92f1c489cbaff3d
lehorseyexpress <162401234+lehorseyexpress@users.noreply.github.com>
2024-11-08 19:36:01 +0000

    Quick-Update (from Replit)

--- auto-save.sh
+++ auto-save.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 save-init () {
+    seconds=$((result - 3))
+
     sleep $seconds
     git add .
     git commit -m "Auto-Update (from Replit)"
     git push origin main
     git pull origin main
+
+    clear
+    sleep 0.5
+    echo "You can turn off auto save by using ctrl+c."
+    sleep 1.5
+
+    save-init
 }
 
-echo "How long until every auto-save in seconds? (5/10/15/20)"
-read seconds
+echo "How long until every auto-save in seconds?"
+read answer
 
 save-init
