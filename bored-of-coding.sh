05c48e8e6f278a24efa672a8fc6c74d5b38165e2
lehorseyexpress <162401234+lehorseyexpress@users.noreply.github.com>
2024-11-12 19:21:06 +0000

    Auto-Update (from Replit)

--- bored-of-coding.sh
+++ bored-of-coding.sh
@@ -26,7 +26,7 @@ elif [ $diff = "hard" ] || [ $diff = "HARD" ]; then
 
     if [ $number = $randomNum ]; then
         echo "Correct! +2 points."
-        
+        # add 2 points to the variable name 'points'
 
 else
     echo "[Err]: Invalid input."
