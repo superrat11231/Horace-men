f38d396f7901cbad20bce70dae20c70f88ff9ef5
lehorseyexpress <162401234+lehorseyexpress@users.noreply.github.com>
2024-11-14 22:16:56 +0000

    Quick-Update (from Replit)

---
+++ animation1.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+cho "Saving...
+                                                      _______"
+sleep 0.2
+clear
+
+echo "Saving...
+                                                      -______"
+sleep 0.2
+clear
+
+echo "Saving...
+                                                      _-_____"
+sleep 0.2
+clear
+
+echo "Saving...
+                                                      __-____"
+sleep 0.2
+clear
+
+git add .
+git commit -m "Quick-Update (from Replit)"
+git push origin main
+git pull origin main
+clear
+
+echo "Saving...
+                                                      _______"
+sleep 0.2
+clear
+
+echo "Saving...
+                                                      -______"
+sleep 0.2
+clear
+
+echo "Saving...
+                                                      _-_____"
+sleep 0.2
+clear
+
+echo "Saving...
+                                                      __-____"
+sleep 0.2
+clear
+
+echo "Saving...
+                                                      ___-___"
+sleep 0.2
+clear
+
+echo "Saving...
+                                                      ____-__"
+sleep 0.2
+clear
+
+echo "Saving...
+                                                      _____-_"
+sleep 0.2
+clear
+
+echo "Saving...
+                                                      ______-"
+sleep 0.2
+clear
+
+echo "Saving...
+                                                      _______"
+sleep 0.2
+clear
+
+echo "Saving...
+                                                      -______"
+sleep 0.2
+clear
+
+echo "Saving...
+                                                      _-_____"
+sleep 0.2
+clear
+
+echo "Saving...
+                                                      __-____"
+sleep 0.2
+clear
+
+echo "Saving...
+                                                      ___-___"
+sleep 0.2
+clear
+
+echo "Saving...
+                                                      ____-__"
+sleep 0.2
+clear
+
+echo "
+                                                      _____-_"
+sleep 0.2
+clear
+
+echo "
+                                                      ______-"
+sleep 0.2
+clear
+
+echo "
+                                                      _______"
+sleep 0.8
+clear
+
+echo "
+                                                      _______"
+sleep 2
+clear
