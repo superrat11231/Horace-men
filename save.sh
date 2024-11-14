919cdd3e122f897b37decdbc91822b86bace5d38
lehorseyexpress <162401234+lehorseyexpress@users.noreply.github.com>
2024-11-14 20:53:25 +0000

    Quick-Update (from Replit)

--- save.sh
+++ save.sh
@@ -26,54 +26,54 @@ git add .
 git commit -m "Quick-Update (from Replit)"
 git push origin main
 git pull origin main
-
 clear
 
-echo "
+echo "Saving...
                                                       ___"
 sleep 0.3
 clear
 
-echo "
+echo "Saving...
                                                       -__"
 sleep 0.3
 clear
 
-echo "
+echo "Saving...
                                                       _-_"
 sleep 0.3
 clear
 
-echo "
+echo "Saving...
                                                       __-"
 sleep 0.3
 clear
 
-echo "
+echo "Saving...
                                                       ___"
 sleep 0.3
 clear
 
-echo "
+echo "Saving...
                                                       -__"
 sleep 0.3
 clear
 
-echo "
+echo "Saving...
                                                       _-_"
 sleep 0.3
 clear
 
-echo "
+echo "Saving...
                                                       __-"
 sleep 0.3
 clear
 
-echo "
+echo "Saving...
                                                       ___"
 sleep 0.8
 
-echo "Succesfully saved!"
+echo "Succesfully saved!
+                                                      ___"
 sleep 2
 clear
 
